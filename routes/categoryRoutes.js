48896f3ef06fe86ff5107db8505504725f212a4e
Teodor <teodorvoicu87@gmail.com>
2024-08-02 19:42:12 +0300

    controller for attributes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getCatergories,
+  getCategories,
   newCategory,
+  deleteCategory,
+  saveAttr,
 } = require('../controllers/categoryController');
 
-router.get('/', getCatergories);
+router.get('/', getCategories);
 router.post('/', newCategory);
+router.delete('/:category', deleteCategory);
+router.post('/attr', saveAttr);
 
 module.exports = router;
