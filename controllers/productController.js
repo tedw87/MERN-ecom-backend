e38ff9657b34aad1fd8e4dcb4a6b903eb04e5fd3
Teodor <teodorvoicu87@gmail.com>
2024-08-06 16:52:39 +0300

    pagination, sorting wip

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,31 @@
-const { recordsPerPage } = require('../config/pagination');
 const Product = require('../models/ProductModel');
+const recordsPerPage = require('../config/pagination');
 
 const getProducts = async (req, res, next) => {
   try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
+
+    // sort by name, price etc.
+    let sort = {};
+    const sortOption = req.query.sort || '';
+    if (sortOption) {
+      let sortOpt = sortOption.split('_');
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+    }
+
     const products = await Product.find({})
-      .sort({ name: 1 })
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort(sort)
       .limit(recordsPerPage);
-    res.json({ products });
+
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
   } catch (error) {
     next(error);
   }
 };
-
 module.exports = getProducts;
